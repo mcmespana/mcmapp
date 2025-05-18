138c46d52d594031f0253e0804dc7f41857b8c1b
mcmespana <admin@movimientoconsolacion.com>
2025-05-18 02:12:22 +0200

    antes notificaciones

--- mcm-app/app/_layout.tsx
+++ mcm-app/app/_layout.tsx
@@ -4,6 +4,11 @@ import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+// Importamos el manejador de notificaciones
+import '../notifications/NotificationHandler';   // 1️⃣ inicializa el handler
+import usePushNotifications from '../notifications/usePushNotifications'; // 2️⃣ nuestro hook
+
+
 export default function RootLayout() {
   const scheme = useColorScheme();
   const theme = scheme === 'dark' ? DarkTheme : DefaultTheme;
