2c33828f20fcc9d67861dac416734fdb394f1568
mcmespana <admin@movimientoconsolacion.com>
2025-06-08 11:59:21 +0200

    aaa

--- mcm-app/components/ui/IconSymbol.tsx
+++ mcm-app/components/ui/IconSymbol.tsx
@@ -18,6 +18,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'close': 'close',
+  'settings': 'settings',
 } as IconMapping;
 
 /**
