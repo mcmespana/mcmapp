dcebd3c3d45f26086277377cdca346949d50472b
mcmespana <admin@movimientoconsolacion.com>
2025-06-23 18:44:32 +0200

    ddd

--- mcm-app/hooks/firebaseApp.ts
+++ mcm-app/hooks/firebaseApp.ts
@@ -1,24 +1,12 @@
 import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform } from 'react-native'; // 👈 importante para detectar si estamos en web o nativo
+import { getAuth } from 'firebase/auth';
 import { firebaseConfig } from '@/constants/firebase';
 
 let app: FirebaseApp;
-let authInitialized = false;
 
 export function getFirebaseApp(): FirebaseApp {
   if (!getApps().length) {
     app = initializeApp(firebaseConfig);
-
-    if (Platform.OS !== 'web') {
-      // Solo inicializar con AsyncStorage en móvil
-      initializeAuth(app, {
-        persistence: getReactNativePersistence(AsyncStorage),
-      });
-    }
-
-    authInitialized = true;
   } else {
     app = getApps()[0];
   }
